475011f478e2268eb0d94631961c7b4969a995ed
Jacobtread <jacobtread@gmail.com>
2024-11-23 03:58:44 +1300

    feat: styled twitch connect ui

--- src/lib/sections/TwitchOAuth.svelte
+++ src/lib/sections/TwitchOAuth.svelte
@@ -11,15 +11,64 @@
   }
 </script>
 
-}
+<main>
+  <div class="container">
+    <h1 class="title">Twitch Login</h1>
+    <p class="message">
+      You are not currently connected to <b>Twitch</b>, please visit the link
+      below to allow access. Click "Open in browser" to open the link in your
+      default browser.
+    </p>
+    {#await getTwitchURL() then twitchURL}
+      <input class="url" type="text" readonly value={twitchURL} />
+    {/await}
 
-<div>
-  <h1>Twitch Login</h1>
-  <p>You are not currently logged in with Twitch</p>
+    <button onclick={openTwitchURL} class="button">Open in browser</button>
+  </div>
+</main>
 
-  {#await getTwitchURL() then twitchURL}
-    <p>{twitchURL}</p>
-  {/await}
+<style>
+  main {
+    display: flex;
+    flex-flow: column;
+    width: 100%;
+    height: 100vh;
+    justify-content: center;
+    align-items: center;
+  }
 
-  <button onclick={openTwitchURL}>Login</button>
-</div>
+  .container {
+    display: flex;
+    flex-flow: column;
+    gap: 1rem;
+    max-width: 34rem;
+  }
+
+  .title {
+    color: #fff;
+  }
+
+  .url {
+    font-size: 1.25rem;
+    padding: 1rem;
+    background-color: #333;
+    border: 1px solid #fff;
+    color: #fff;
+    border-radius: 0.25rem;
+  }
+
+  .button {
+    font-size: 1rem;
+    padding: 1rem;
+    background-color: #712880;
+    border: none;
+    color: #fff;
+    border-radius: 0.25rem;
+    align-self: flex-start;
+  }
+
+  .button:hover {
+    background-color: #9731ac;
+    cursor: pointer;
+  }
+</style>
