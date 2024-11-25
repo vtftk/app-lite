c919c37ee09a2f078f70febc81c6f38a9a6d016a
Jacobtread <jacobtread@gmail.com>
2024-11-25 19:34:08 +1300

    feat: overlay compile to single file

--- overlay/vite.config.js
+++ overlay/vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from "vite";
 import path from "path";
+import { viteSingleFile } from "vite-plugin-singlefile";
 
 export default defineConfig(async () => ({
+  plugins: [viteSingleFile()],
   resolve: {
     alias: {
       $shared: path.resolve(__dirname, "../types"),
