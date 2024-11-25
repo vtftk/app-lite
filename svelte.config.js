280ed8f077af4c9d48aefed5fd70099a0c627b76
Jacobtread <jacobtread@gmail.com>
2024-11-25 20:34:04 +1300

    feat: overlay embed single file

--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,9 @@ import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 const config = {
   preprocess: vitePreprocess(),
   kit: {
-    adapter: adapter(),
+    adapter: adapter({
+      fallback: "index.html",
+    }),
     alias: {
       $shared: "./types",
     },
