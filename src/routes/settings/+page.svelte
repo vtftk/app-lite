049619e6a2223f89b1e0660ba9707e92ec140b44
Jacobtread <jacobtread@gmail.com>
2024-11-29 16:34:49 +1300

    feat: add empty pages for settings and scripts

---
+++ src/routes/settings/+page.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+</script>
+
+<div class="container">
+  <div class="title-area">
+    <div>
+      <h1 class="title">Settings</h1>
+      <p class="text">Configuration for the entire app</p>
+    </div>
+  </div>
+</div>
+
+<style>
+  .container {
+    display: flex;
+    flex-flow: column;
+    gap: 0.5rem;
+
+    padding: 1rem;
+    height: 100%;
+  }
+
+  .title {
+    line-height: 1;
+  }
+
+  .title-area {
+    display: flex;
+  }
+</style>
