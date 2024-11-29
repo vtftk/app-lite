425e90b637b58ce0a7eb54f84d4fe4afcf2fa43f
Jacobtread <jacobtread@gmail.com>
2024-11-30 00:40:42 +1300

    feat: code editor

---
+++ src/lib/components/CodeEditor.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+  import { onDestroy, onMount } from "svelte";
+  import type * as Monaco from "monaco-editor/esm/vs/editor/editor.api";
+
+  let editor: Monaco.editor.IStandaloneCodeEditor;
+  let monaco: typeof Monaco;
+  let editorContainer: HTMLElement | undefined = $state();
+
+  onMount(async () => {
+    monaco = (await import("$lib/monaco")).default;
+
+    // Your monaco instance is ready, let's display some code!
+    editor = monaco.editor.create(editorContainer!, {
+      theme: "vs-dark",
+      automaticLayout: true,
+    });
+    const model = monaco.editor.createModel("", "javascript");
+    editor.setModel(model);
+  });
+
+  onDestroy(() => {
+    monaco?.editor.getModels().forEach((model) => model.dispose());
+    editor?.dispose();
+  });
+</script>
+
+<svelte:window
+  onresize={() => {
+    if (!editor) return;
+    // make editor as small as possible
+    editor.layout({ width: 0, height: 0 });
+
+    // wait for next frame to ensure last layout finished
+    window.requestAnimationFrame(() => {
+      if (!editorContainer) return;
+      const rect = editorContainer.getBoundingClientRect();
+      editor.layout({ width: rect.width, height: rect.height });
+    });
+  }}
+/>
+
+<div class="container" bind:this={editorContainer}></div>
+
+<style>
+  .container {
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+  }
+</style>
--- src/routes/scripts/+page.svelte
+++ src/routes/scripts/+page.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
+  import CodeEditor from "$lib/components/CodeEditor.svelte";
   import PageLayoutList from "$lib/layouts/PageLayoutList.svelte";
 </script>
 
 <PageLayoutList
   title="Scripts"
   description="Create custom scripts that can be triggered"
-></PageLayoutList>
+>
+  <CodeEditor />
+</PageLayoutList>
