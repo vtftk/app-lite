1369826caec4c1909d4f65bdda169e6f83499066
Jacobtread <jacobtread@gmail.com>
2024-12-28 00:58:54 +1300

    fix: scoping for userFunction wrapper

--- script/wrapper_call.js
+++ script/wrapper_call.js
@@ -1,14 +1,16 @@
-const userFunction = async (event) => {
-  // Run user code
-  try {
-    USER_CODE;
-  } catch (err) {
-    console.error("error running user event script code", err);
-  }
-};
+(() => {
+  const userFunction = async (event) => {
+    // Run user code
+    try {
+      USER_CODE;
+    } catch (err) {
+      console.error("error running user event script code", err);
+    }
+  };
 
-async (ctx, data) => {
-  api.logging.runWithContext(ctx, async () => {
-    await userFunction(data);
-  });
-};
+  return async (ctx, data) => {
+    api.logging.runWithContext(ctx, async () => {
+      await userFunction(data);
+    });
+  };
+})();
--- script/wrapper_command.js
+++ script/wrapper_command.js
@@ -1,29 +1,31 @@
-const userFunction = async (ctx) => {
-  try {
-    USER_CODE;
-  } catch (err) {
-    console.error("error running user command code", err);
-  }
-};
-
-async (ctx, cmd_ctx) => {
-  api.logging.runWithContext(ctx, async () => {
-    const commandCtx = {
-      ...cmd_ctx,
-
-      // Inject getters for helping with getting the target user
-      get targetUser() {
-        return api.twitch.getUsernameArg(cmd_ctx.args[0], false);
-      },
-
-      get targetUserValid() {
-        return api.twitch.getUsernameArg(cmd_ctx.args[0], true);
-      },
-    };
-
-    const value = await userFunction(commandCtx);
-    if (typeof value === "string") {
-      await api.twitch.sendChat(value);
+(() => {
+  const userFunction = async (ctx) => {
+    try {
+      USER_CODE;
+    } catch (err) {
+      console.error("error running user command code", err);
     }
-  });
-};
+  };
+
+  return async (ctx, cmd_ctx) => {
+    api.logging.runWithContext(ctx, async () => {
+      const commandCtx = {
+        ...cmd_ctx,
+
+        // Inject getters for helping with getting the target user
+        get targetUser() {
+          return api.twitch.getUsernameArg(cmd_ctx.args[0], false);
+        },
+
+        get targetUserValid() {
+          return api.twitch.getUsernameArg(cmd_ctx.args[0], true);
+        },
+      };
+
+      const value = await userFunction(commandCtx);
+      if (typeof value === "string") {
+        await api.twitch.sendChat(value);
+      }
+    });
+  };
+})();
