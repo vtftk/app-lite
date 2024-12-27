f276249e8dad85653b9f3a54c5acc690754b4787
Jacobtread <jacobtread@gmail.com>
2024-12-28 02:34:33 +1300

    fix: followage account for minutes and seconds

--- examples/commands/followage.js
+++ examples/commands/followage.js
@@ -5,7 +5,7 @@ let userId = user.id;
 
 if (targetUserValid !== null) {
   // Find the twitch user
-  const twitchUser = await api.twitch.getUserByUsername(targetUser);
+  const twitchUser = await api.twitch.getUserByUsername(targetUserValid);
 
   userId = twitchUser.id;
   userName = twitchUser.name;
@@ -24,8 +24,24 @@ let years = endDate.getFullYear() - startDate.getFullYear();
 let months = endDate.getMonth() - startDate.getMonth();
 let days = endDate.getDate() - startDate.getDate();
 let hours = endDate.getHours() - startDate.getHours();
+let minutes = endDate.getMinutes() - startDate.getMinutes();
+let seconds = endDate.getSeconds() - startDate.getSeconds();
 
-// Adjust months and years if necessary
+if (seconds < 0) {
+  seconds += 60;
+  minutes--;
+}
+
+if (minutes < 0) {
+  minutes += 60;
+  hours--;
+}
+
+if (hours < 0) {
+  hours += 24;
+  days--;
+}
+
 if (days < 0) {
   months -= 1;
   const previousMonth = new Date(endDate.getFullYear(), endDate.getMonth(), 0);
@@ -42,7 +58,7 @@ const parts = [];
 if (years > 0) parts.push(`${years} year${years > 1 ? "s" : ""}`);
 if (months > 0) parts.push(`${months} month${months > 1 ? "s" : ""}`);
 if (days > 0) parts.push(`${days} day${days > 1 ? "s" : ""}`);
-if (hours > 0) parts.push(`${hours} hours${hours > 1 ? "s" : ""}`);
+if (hours > 0) parts.push(`${hours} hour${hours > 1 ? "s" : ""}`);
 
 const dateDistance = parts.length > 0 ? parts.join(", ") : "0 hours";
 
