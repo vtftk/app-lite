71cc6f30997e085fc23470dbfd5d0869d2db4a1a
Jacobtread <jacobtread@gmail.com>
2024-12-31 00:40:28 +1300

    fix: example command embed

--- src/lib/sections/commands/CommandForm.svelte
+++ src/lib/sections/commands/CommandForm.svelte
@@ -52,8 +52,8 @@ const data = JSON.parse(response.response);
 const title = data.title;
 
 const randomNumber = Math.floor(Math.random() * 100000);
-return `You said: "${message}"; Heres a random title: ${title}, Heres a random number: ${randomNumber}`;
-  `
+return \`You said: "\${message}"; Heres a random title: \${title}, Heres a random number: \${randomNumber}\`;
+  `;
 
   type Props = {
     existing?: Command;
