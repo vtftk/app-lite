f0b6da2ab8a73e751d00897f82994b3fb2b66be2
Jacobtread <jacobtread@gmail.com>
2025-01-03 01:52:19 +1300

    fix: simplified example command

--- src/lib/sections/commands/CommandForm.svelte
+++ src/lib/sections/commands/CommandForm.svelte
@@ -35,24 +35,16 @@
   import CommandExecutions from "./CommandExecutions.svelte";
   import RequiredRoleSelect from "../events/RequiredRoleSelect.svelte";
 
-  const exampleCode = `
-const message = ctx.message;
+  const exampleCode = `const { targetUser } = ctx;
 
-const response = await api.http.get(
-  "https://jsonplaceholder.typicode.com/todos/1"
-);
+// Pick a random message
+const message = [
+  \`\${targetUser} has been chosen\`,
+  \`you picked \${targetUser}\`
+].random();
 
-// Failed to get a response
-if (!response.ok) {
-  await api.twitch.sendChat("Failed to retrieve response from API");
-  return;
-}
-
-const data = JSON.parse(response.response);
-const title = data.title;
-
-const randomNumber = Math.floor(Math.random() * 100000);
-return \`You said: "\${message}"; Heres a random title: \${title}, Heres a random number: \${randomNumber}\`;
+// Return the message to say it
+return message;
   `;
 
   type Props = {
