5970f2272227b2d75e0859e12eaf13ac6c968501
Jacobtread <jacobtread@gmail.com>
2025-01-05 17:37:31 +1300

    feat: apply terser to runtime cleanup rollup config

--- scripting/rollup.config.mjs
+++ scripting/rollup.config.mjs
@@ -3,31 +3,45 @@ import path from 'node:path';
 import { globSync } from 'glob';
 import { dts } from "rollup-plugin-dts";
 import { fileURLToPath } from 'node:url';
+import terser from '@rollup/plugin-terser';
 import typescript from '@rollup/plugin-typescript';
 
 import fixGlobalFile from './fix-global-file.mjs';
 
+/**
+ * Helper to get all the .test.ts files for compiling tests
+ * 
+ * @returns The list of test input files
+ */
+function getTestFiles() {
+    return Object.fromEntries(globSync('src/**/*.test.ts').map(file => [
+        // This remove `src/` as well as the file extension from each
+        // file, so e.g. src/nested/foo.js becomes nested/foo
+        path.relative(
+            'src',
+            file.slice(0, file.length - path.extname(file).length)
+        ),
+        // This expands the relative paths to absolute paths, so e.g.
+        // src/nested/foo becomes /project/src/nested/foo.js
+        fileURLToPath(new URL(file, import.meta.url))
+    ]));
+}
+
+
 export default [
+    // Main runtime bundle
     {
         input: 'src/runtime.ts',
         output: {
             file: 'dist/runtime.js',
             format: 'es'
         },
-        plugins: [typescript()]
+        plugins: [typescript(), terser()]
     },
+
+    // Compiled test javascript
     {
-        input: Object.fromEntries(globSync('src/**/*.test.ts').map(file => [
-            // This remove `src/` as well as the file extension from each
-            // file, so e.g. src/nested/foo.js becomes nested/foo
-            path.relative(
-                'src',
-                file.slice(0, file.length - path.extname(file).length)
-            ),
-            // This expands the relative paths to absolute paths, so e.g.
-            // src/nested/foo becomes /project/src/nested/foo.js
-            fileURLToPath(new URL(file, import.meta.url))
-        ])),
+        input: getTestFiles(),
         output: {
             dir: 'dist',
             format: 'es'
@@ -35,6 +49,8 @@ export default [
         plugins: [typescript()],
         external: ['jest'],
     },
+
+    // Bundle typescript definitions
     {
         input: "dist/types/runtime.d.ts",
         output: [{ file: "dist/runtime.d.ts", format: "es" }],
