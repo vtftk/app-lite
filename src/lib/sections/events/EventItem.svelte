f433888e0ab940da6585d113b93e547da9178056
Jacobtread <jacobtread@gmail.com>
2025-01-10 22:36:42 +1300

    feat: add event type indicators to event list items

--- src/lib/sections/events/EventItem.svelte
+++ src/lib/sections/events/EventItem.svelte
@@ -1,6 +1,4 @@
 <script lang="ts">
-  import type { VEvent } from "$lib/api/types";
-
   import { toast } from "svelte-sonner";
   import { deleteEvent } from "$lib/api/vevents";
   import { toastErrorMessage } from "$lib/utils/error";
@@ -9,10 +7,30 @@
   import Button from "$lib/components/input/Button.svelte";
   import SolarMenuDotsBold from "~icons/solar/menu-dots-bold";
   import LinkButton from "$lib/components/input/LinkButton.svelte";
+  import SolarGiftBoldDuotone from "~icons/solar/gift-bold-duotone";
   import PopoverButton from "$lib/components/popover/PopoverButton.svelte";
+  import SolarKeyboardBoldDuotone from "~icons/solar/keyboard-bold-duotone";
   import { confirmDialog } from "$lib/components/GlobalConfirmDialog.svelte";
+  import SolarMoneyBagBoldDuotone from "~icons/solar/money-bag-bold-duotone";
+  import SolarStopwatchBoldDuotone from "~icons/solar/stopwatch-bold-duotone";
+  import SolarHandMoneyBoldDuotone from "~icons/solar/hand-money-bold-duotone";
+  import SolarHandHeartBoldDuotone from "~icons/solar/hand-heart-bold-duotone";
+  import SolarBasketballBoldDuotone from "~icons/solar/basketball-bold-duotone";
+  import SolarBoltCircleBoldDuotone from "~icons/solar/bolt-circle-bold-duotone";
+  import SolarTextSquareBoldDuotone from "~icons/solar/text-square-bold-duotone";
+  import SolarCodeSquareBoldDuotone from "~icons/solar/code-square-bold-duotone";
+  import SolarArrowRightBoldDuotone from "~icons/solar/arrow-right-bold-duotone";
   import ControlledCheckbox from "$lib/components/input/ControlledCheckbox.svelte";
-
+  import SolarSkateboardingBoldDuotone from "~icons/solar/skateboarding-bold-duotone";
+  import SolarChatSquareCodeBoldDuotone from "~icons/solar/chat-square-code-bold-duotone";
+  import SolarEmojiFunnyCircleBoldDuotone from "~icons/solar/emoji-funny-circle-bold-duotone";
+  import SolarUsersGroupRoundedBoldDuotone from "~icons/solar/users-group-rounded-bold-duotone";
+  import {
+    type VEvent,
+    EventOutcomeType,
+    EventTriggerType,
+  } from "$lib/api/types";
+  import SolarHeadphonesRoundSoundBoldDuotone from "~icons/solar/headphones-round-sound-bold-duotone";
   type Props = {
     config: VEvent;
 
@@ -50,21 +68,118 @@
 {/snippet}
 
 <div class="event">
-  <ControlledCheckbox checked={selected} onCheckedChange={onToggleSelected} />
+  <div class="event__base">
+    <ControlledCheckbox checked={selected} onCheckedChange={onToggleSelected} />
 
-  <div class="event__content">
-    <a title={config.name} href="/events/{config.id}" class="event__name">
-      {config.name}
-    </a>
+    <div class="event__content">
+      <a title={config.name} href="/events/{config.id}" class="event__name">
+        {config.name}
+      </a>
+    </div>
+
+    <div class="action">
+      <PopoverButton
+        content={popoverContent}
+        contentProps={{ align: "start", side: "left" }}
+      >
+        <SolarMenuDotsBold />
+      </PopoverButton>
+    </div>
   </div>
 
-  <div class="action">
-    <PopoverButton
-      content={popoverContent}
-      contentProps={{ align: "start", side: "left" }}
-    >
-      <SolarMenuDotsBold />
-    </PopoverButton>
+  <div class="event__detail">
+    {#if config.trigger.type === EventTriggerType.Redeem}
+      <div class="detail" data-color="purple">
+        <SolarBoltCircleBoldDuotone />
+        Redeem
+      </div>
+    {:else if config.trigger.type === EventTriggerType.Command}
+      <div class="detail" data-color="red">
+        <SolarTextSquareBoldDuotone />
+        Command
+      </div>
+    {:else if config.trigger.type === EventTriggerType.Follow}
+      <div class="detail" data-color="yellow">
+        <SolarUsersGroupRoundedBoldDuotone />
+        Follow
+      </div>
+    {:else if config.trigger.type === EventTriggerType.Subscription}
+      <div class="detail" data-color="green">
+        <SolarUsersGroupRoundedBoldDuotone />
+        Subscription
+      </div>
+    {:else if config.trigger.type === EventTriggerType.GiftedSubscription}
+      <div class="detail" data-color="blue">
+        <SolarGiftBoldDuotone />
+        Gifted Subscription
+      </div>
+    {:else if config.trigger.type === EventTriggerType.Bits}
+      <div class="detail" data-color="purple">
+        <SolarHandMoneyBoldDuotone />
+        Gifted Bits
+      </div>
+    {:else if config.trigger.type === EventTriggerType.Raid}
+      <div class="detail" data-color="red">
+        <SolarSkateboardingBoldDuotone />
+        Raid
+      </div>
+    {:else if config.trigger.type === EventTriggerType.Timer}
+      <div class="detail" data-color="green">
+        <SolarStopwatchBoldDuotone />
+        Timer
+      </div>
+    {:else if config.trigger.type === EventTriggerType.AdBreakBegin}
+      <div class="detail" data-color="blue">
+        <SolarMoneyBagBoldDuotone />
+        Ad Break Started
+      </div>
+    {:else if config.trigger.type === EventTriggerType.ShoutoutReceive}
+      <div class="detail" data-color="purple">
+        <SolarHandHeartBoldDuotone />
+        Shoutout Received
+      </div>
+    {/if}
+
+    {#if config.outcome.type === EventOutcomeType.ThrowBits}
+      <div class="detail" data-color="green">
+        <SolarHandMoneyBoldDuotone />
+        Throw Bits
+      </div>
+    {:else if config.outcome.type === EventOutcomeType.ChannelEmotes}
+      <div class="detail" data-color="yellow">
+        <SolarEmojiFunnyCircleBoldDuotone />
+        Channel Emotes
+      </div>
+    {:else if config.outcome.type === EventOutcomeType.Throwable}
+      <div class="detail" data-color="purple">
+        <SolarBasketballBoldDuotone />
+        Throw Item
+      </div>
+    {:else if config.outcome.type === EventOutcomeType.TriggerHotkey}
+      <div class="detail" data-color="red">
+        <SolarKeyboardBoldDuotone />
+        Trigger Hotkey
+      </div>
+    {:else if config.outcome.type === EventOutcomeType.PlaySound}
+      <div class="detail" data-color="yellow">
+        <SolarHeadphonesRoundSoundBoldDuotone />
+        Play Sound
+      </div>
+    {:else if config.outcome.type === EventOutcomeType.SendChatMessage}
+      <div class="detail" data-color="green">
+        <SolarChatSquareCodeBoldDuotone />
+        Send chat message
+      </div>
+    {:else if config.outcome.type === EventOutcomeType.Script}
+      <div class="detail" data-color="purple">
+        <SolarCodeSquareBoldDuotone />
+        Run script
+      </div>
+    {/if}
+
+    <span class="detail-transition">
+      <SolarArrowRightBoldDuotone />
+    </span>
   </div>
 </div>
 
@@ -75,14 +190,31 @@
     border-radius: 5px;
 
     display: flex;
-    justify-content: flex-start;
-    align-items: center;
-    gap: 1rem;
+    flex-flow: column;
 
     padding: 0.5rem;
     overflow: hidden;
+    gap: 0.5rem;
 
+    height: 100px;
+  }
+
+  .event__base {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    width: 100%;
     height: 60px;
+    gap: 1rem;
+  }
+
+  .event__detail {
+    display: flex;
+    display: grid;
+    grid-template-columns: repeat(2, minmax(0, 1fr));
+    gap: 0.5rem;
+    height: 40px;
+    position: relative;
   }
 
   .event__name {
@@ -108,5 +240,64 @@
 
   .action {
     flex-shrink: 0;
+  }
+
+  .detail-transition {
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    color: #999;
+    font-size: 0.9rem;
+    transform: translate(-50%, -50%);
+  }
+
+  .detail {
+    opacity: 0.75;
+    display: flex;
+    gap: 0.5rem;
+    margin: 0 0.2rem;
+    background-color: #222;
+    border: 1px solid #111;
+    color: #ccc;
+    font-size: 0.9rem;
+    font-weight: normal;
+
+    border-radius: 0.25rem;
+    padding: 0.25rem 0.75rem;
+
+    vertical-align: middle;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+
+  .detail[data-color="purple"] {
+    border-color: #dd82f0;
+    background-color: #3c1b42;
+    color: #dd82f0;
+  }
+
+  .detail[data-color="red"] {
+    border-color: #f08282;
+    background-color: #421b1b;
+    color: #f08282;
+  }
+
+  .detail[data-color="yellow"] {
+    border-color: #eef082;
+    background-color: #423f1b;
+    color: #f0ee82;
+  }
+
+  .detail[data-color="green"] {
+    border-color: #a1f082;
+    background-color: #1b421b;
+    color: #a1f082;
+  }
+
+  .detail[data-color="blue"] {
+    border-color: #82bbf0;
+    background-color: #1b2f42;
+    color: #82bbf0;
   }
 </style>
